efa196e84ad36c80e3fd0676b352838cd0c31271
cynamon jules <julescweb@mail.com>
2022-05-12 12:37:38 +0200

    petite correction

--- auth-react-firebase/src/firebase-config.js
+++ auth-react-firebase/src/firebase-config.js
@@ -1,11 +1,11 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth';
+import { getAuth } from "firebase/auth";
 
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: process.env.REACT_APPFIREBASE__KEY,
-	authDomain: process.env.REACT_APPFIREBASE__AUTH_DOMAIN,
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
 	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
 	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
